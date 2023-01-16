0df062699d29ae4128d59eb335afc52029f9d5b5
joo <sejuyes@gmail.com>
2023-01-16 18:22:38 +0900

    Feat: react-router 세팅하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Front from "./components/Front/Front";
 import Home from "./components/Home/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/front" element={<Front />}></Route>
+        {/* <Route path="*" element={<NotFound />}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
