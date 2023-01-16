338fca472ded38978e4448cbcdf160f9b19b49fc
joo <sejuyes@gmail.com>
2023-01-16 17:58:02 +0900

    Feat: Home 마크업 및 디자인

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Navbar from "./Navbar";
 import hotel from "../../images/hotel_main.png";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <section>
-        <div>
+        <div className={styles.content}>
           <h1>기억의 궁전</h1>
           <p>셜록의 기억의 궁전처럼 기록을 저장하는 공간입니다.</p>
           <button>입장하기</button>
