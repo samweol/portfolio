30cc9c5f66c31beee7eaec1aa7f278656588c40e
joo <sejuyes@gmail.com>
2023-01-17 15:09:30 +0900

    Style: 홈화면 스타일링, navbar 스타일링

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,8 +16,8 @@ export default function Home() {
           <p>셜록의 기억의 궁전처럼 기록을 저장하는 공간입니다.</p>
           <button onClick={enterClick}>입장하기</button>
         </div>
-        <div>
-          <img src={hotel} alt="호텔 이미지" width={"100%"} />
+        <div className={styles.hotel}>
+          <img src={hotel} alt="호텔 이미지" width={"50%"} />
         </div>
       </section>
     </>
