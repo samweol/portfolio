60085a5601183b7c2e7445c35706ba93214ff435
joo <sejuyes@gmail.com>
2023-01-17 15:52:08 +0900

    Feat: 편지 쓰기 페이지 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Front from "./components/Front/index";
 import Home from "./components/Home/Home";
 import Library from "./components/Library";
 import MessageBox from "./components/MessageBox";
+import WirteMessage from "./components/MessageBox/WriteMessage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/front" element={<Front />}></Route>
         <Route path="/message" element={<MessageBox />}></Route>
         <Route path="/library" element={<Library />}></Route>
+        <Route path="/message/write" element={<WirteMessage />}></Route>
         {/* <Route path="*" element={<NotFound />}></Route> */}
       </Routes>
     </BrowserRouter>
---
+++ src/components/MessageBox/WriteMessage/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WirteMessage() {
+  return <div>WriteMessage Area</div>;
+}
