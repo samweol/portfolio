97abca5a1c6499048ab6911aaa7a054f41c15de6
joo <sejuyes@gmail.com>
2023-01-17 17:48:18 +0900

    Feat: firebase와 연동해서 편지쓰기 기능 구현(디자인 미완)

--- src/components/MessageBox/WriteMessage/index.jsx
+++ src/components/MessageBox/WriteMessage/index.jsx
@@ -1,5 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../../Home/Navbar";
+import styles from "./WriteMessage.module.css";
+import { set, ref } from "firebase/database";
+import { database } from "../../../firebase";
+import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 export default function WirteMessage() {
-  return <div>WriteMessage Area</div>;
+  const [data, setData] = useState({
+    saver: "",
+    content: "",
+    sender: "",
+    writeAt: new Date(),
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    set(ref(database, "letters/" + uuidv4()), {
+      saver: data.saver,
+      content: data.content,
+      sender: data.sender,
+      writeAt: data.writeAt.toLocaleDateString(),
+    });
+    console.log("💌 편지가 전달되었습니다.");
+    navigate("/message");
+  };
+  return (
+    <>
+      <Navbar />
+      <section>
+        <form className={styles.section}>
+          <div>
+            To.{" "}
+            <input
+              type="text"
+              onChange={(s) => {
+                setData({ ...data, saver: s.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className={styles.textarea}
+              onChange={(c) => {
+                setData({ ...data, content: c.target.value });
+              }}
+            ></textarea>
+          </div>
+          <div>
+            From.{" "}
+            <input
+              type="text"
+              onChange={(s) => {
+                setData({ ...data, sender: s.target.value });
+              }}
+            />
+          </div>
+          <button onClick={onSubmit}>편지 보내기</button>
+        </form>
+      </section>
+    </>
+  );
 }
--- src/components/MessageBox/index.jsx
+++ src/components/MessageBox/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../Home/Navbar";
 import Message from "./Message";
 import styles from "./MessageBox.module.css";
 
 export default function MessageBox() {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar />
@@ -15,6 +17,15 @@ export default function MessageBox() {
         <Message content="hello" sender="xxx" />
         <Message content="hello" sender="xxx" />
         <Message content="hello" sender="xxx" />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            navigate("/message/write");
+          }}
+        >
+          편지 쓰기
+        </button>
       </div>
     </>
   );
