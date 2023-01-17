5bcf58a002f3935aa22b8954dedbea1bdef1d1bc
joo <sejuyes@gmail.com>
2023-01-17 14:29:04 +0900

    Feat: 편지쓰기 페이지 추가 및 navigate 설정

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Front from "./components/Front/index";
 import Home from "./components/Home/Home";
 import MessageBox from "./components/MessageBox";
+import WriteMessage from "./components/MessageBox/WriteMessage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/front" element={<Front />}></Route>
         <Route path="/message" element={<MessageBox />}></Route>
+        <Route path="/message/write" element={<WriteMessage />}></Route>
         {/* <Route path="*" element={<NotFound />}></Route> */}
       </Routes>
     </BrowserRouter>
--- src/components/MessageBox/index.jsx
+++ src/components/MessageBox/index.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Navbar from "../Home/Navbar";
 import Message from "./Message";
 import styles from "./MessageBox.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function MessageBox() {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar />
@@ -16,6 +18,14 @@ export default function MessageBox() {
         <Message content="hello" sender="xxx" />
         <Message content="hello" sender="xxx" />
       </div>
+      <button
+        className={styles.writeBtn}
+        onClick={() => {
+          navigate("/message/write");
+        }}
+      >
+        편지 쓰기
+      </button>
     </>
   );
 }
